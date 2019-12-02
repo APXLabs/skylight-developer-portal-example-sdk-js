16730b73963020b2ca3ce7c266b1bb4481124fd8
Andrew Sugaya <andrew.sugaya@upskill.io>
2019-12-02 14:39:02 -0500

    Preparing 1.0.2 tag

--- Examples/UserCreate/program.js
+++ Examples/UserCreate/program.js
@@ -1,6 +1,7 @@
 Skylight.Init();
 //@skydocs.start(users.create)
-//Version 1.0.1 of the JS SDK
-var user = new User("Taylor", "Korpa", "taylor.korpa");
+//Version 1.0.2 of the JS SDK
+var user = new User("Taylor", "Korpa");
+//Alternative: var user = new User("Taylor", "Korpa", "taylor.korpa")
 Skylight.CreateNewUser(user);
 //@skydocs.end()
