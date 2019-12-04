94ff618fcf886a871c95efda37331f411fa04848
asugaya-apx <andrew.sugaya@upskill.io>
2019-12-04 09:13:02 -0500

    Update program.js

--- Examples/UserCreate/program.js
+++ Examples/UserCreate/program.js
@@ -5,3 +5,10 @@ var user = new User("Taylor", "Korpa");
 //Alternative: var user = new User("Taylor", "Korpa", "taylor.korpa")
 Skylight.CreateNewUser(user);
 //@skydocs.end()
+
+//@skydocs.start(users.create)
+//Version 1.0.2 of the JS SDK
+var user = new User("Sally", "Storo");
+//Alternative: var user = new User("Sally", "Storo", "sally.storo")
+Skylight.CreateNewUser(user);
+//@skydocs.end()
